01734fff30e116c964c3e3a12c687c0efedb7a78
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-05 14:10:55 +0700

    Reset Handler Reset Processor, PHP Logging

---
+++ 10-reset-handler-reset-processor/project/tests/LoggingTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AriefKarditya\LocalDomainPhp;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Test\TestCase;
+
+class LoggingTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function logging()
+    {
+        $logger = new Logger(LoggingTest::class);
+        $logger->pushHandler(new StreamHandler("php://stderr"));
+        $logger->pushHandler(new StreamHandler(__DIR__ . "/../application.log"));
+
+        $logger->info("Selamat Belajar PHP Logging");
+
+        TestCase::assertNotNull($logger);
+    }
+}
